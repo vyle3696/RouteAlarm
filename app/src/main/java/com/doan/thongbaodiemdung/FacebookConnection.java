2ec1c7b8112675092951465cb144c53f0abe372e
vyle3696 <vyle3696@gmail.com>
2017-04-16 18:35:11 +0700

    thêm firebase
    kết nối ứng dụng vơi firebase

--- app/src/main/java/com/doan/thongbaodiemdung/FacebookConnection.java
+++ app/src/main/java/com/doan/thongbaodiemdung/FacebookConnection.java
@@ -2,8 +2,10 @@ package com.doan.thongbaodiemdung;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.doan.thongbaodiemdung.Activity.MainActivity;
@@ -13,8 +15,19 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by VYLE on 04/04/2017.
@@ -22,14 +35,24 @@ import com.facebook.login.widget.LoginButton;
 
 public class FacebookConnection extends AppCompatActivity {
 
+    private static final String TAG ="" ;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
     private AccessTokenTracker accessTokenTracker;
 
+    //doc thong tin nguoi dung
+    ProfileTracker profileTracker;
+    Profile profile;
+
+    //thao tac voi firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
@@ -39,8 +62,10 @@ public class FacebookConnection extends AppCompatActivity {
 
             @Override
             public void onSuccess(LoginResult loginResult) {
+
                 Intent mainIntent = new Intent(FacebookConnection.this, MainActivity.class);
                 FacebookConnection.this.startActivity(mainIntent);
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -62,11 +87,39 @@ public class FacebookConnection extends AppCompatActivity {
         };
         accessTokenTracker.startTracking();
 
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile)
+            {
+                // Lưu profile và trích xuất thông tin từ nó
+                profile = currentProfile;
+                setUserInfo(profile.getId());
+
+            }
+        };
+
         if (isLoggedIn()){
             Intent mainIntent = new Intent(FacebookConnection.this, MainActivity.class);
             FacebookConnection.this.startActivity(mainIntent);
 
         }
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
 
     @Override
@@ -79,5 +132,52 @@ public class FacebookConnection extends AppCompatActivity {
         return accessToken != null;
     }
 
-}
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(FacebookConnection.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    public void setUserInfo(String ID)
+    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue(ID);
+    }
+
+}
